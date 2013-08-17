b2d42ebe2621b35eeeb480fe8e8a0d98cad17ffd
eternalmatt <eternalmatt@gmail.com>
2013-08-17 13:37:00 -0400

    Sliders adjust based on other sliders values

--- js/controllers.js
+++ js/controllers.js
@@ -2,5 +2,23 @@ function CharityController($scope, $http){
   $http.get('data/data.json').success(function(data){
     $scope.charities = data.charities;
     $scope.donation = data.donation;
+    $scope.charities.forEach(function(elem, index, array){
+      elem.amount = 1 / array.length; 
+      elem.prevAmount = elem.amount;
+    });
   });
+
+  $scope.change = function(charity){
+    var delta = charity.amount - charity.prevAmount;
+    var length = $scope.charities.length;
+    charity.prevAmount = charity.amount;
+    
+    $scope.charities.forEach(function(elem){
+      if (elem === charity){
+        return;
+      }
+      elem.prevAmount = elem.amount;
+      elem.amount -= delta / length;
+    });
+  };
 }
